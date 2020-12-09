dddc3d0d4afb73684b134274dbff487959b727dc
Mimma Akter <mimma.omi@hotmail.com>
2020-12-09 07:27:14 +0600

    1 commit

---
+++ PGDIT_TPS/Models/Common/connection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PGDIT_TPS.Models.Common
+{
+    public class connection
+    {
+        //create a common class;
+    }
+}
